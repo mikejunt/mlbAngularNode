07c17932909a6d31270379b78fa288868c1ce2ae
mikejunt <mikejunt@gmail.com>
2020-02-11 16:18:27 -0600

    basic structure of user service for login

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { User } from './interfaces/user.interface';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  userlist: Array<User> = [
+    {username: "Mike", password: "password", favteam: "119" }, 
+    {username: "Sarah", password: "password", favteam: "134" }, 
+    {username: "Justin", password: "password", favteam: "137" }]
 
   constructor() { }
+
+  authenticate (username: string, password: string) {
+    let loginattempt = this.userlist.filter(obj => obj.username === username && obj.password === password);
+    console.log(loginattempt)
+    if (loginattempt.length !== 1) {
+      console.log("Username or password incorrect.")
+    }
+    else {
+      localStorage.setItem(JSON.stringify(loginattempt[0]), JSON.stringify(loginattempt[2]))
+      console.log(localStorage)
+      console.log("Successful login things like localStorage & redirect should happen now.")
+    }
+  }
 }
