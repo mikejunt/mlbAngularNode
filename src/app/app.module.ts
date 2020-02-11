0d4ffddfe9daed15aa0791c068641347c440cfc2
mikejunt <mikejunt@gmail.com>
2020-02-11 15:39:29 -0600

    initial creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserheaderComponent } from './userheader/userheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserheaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/userheader/userheader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userheader',
+  templateUrl: './userheader.component.html',
+  styleUrls: ['./userheader.component.scss']
+})
+export class UserheaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
