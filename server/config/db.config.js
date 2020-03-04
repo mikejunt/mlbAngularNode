63498cbf2e8a2229073203304b96002c69425f59
mikejunt <mikejunt@gmail.com>
2020-03-04 10:32:23 -0600

    cleans up unneeded logging

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -6,8 +6,6 @@ const client = new Client(connectionString);
 const pool = new Pool(connectionString);
 
 pool.on('error', (err,client) => console.log("Pool error:", err))
-pool.on('acquire',(client)=>  console.log("Client acquired from pool by query."))
-pool.on('connect', (client) => console.log("New client connected to backend."))
 
 module.exports.client = client
 module.exports.pool = pool
