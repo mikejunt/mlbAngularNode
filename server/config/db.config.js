36a8aeb0433aaa068d58890c60cb43d6e3d7441a
mikejunt <mikejunt@gmail.com>
2020-03-11 14:22:43 -0500

    fixes issue in pool config

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,9 +1,15 @@
 const { Pool, Client } = require('pg')
 const connectionString = process.env.DATABASE_URL
 
-const client = new Client(connectionString);
+const client = new Client({connectionString: connectionString,
+                           ssl: true});
 
-const pool = new Pool(connectionString);
+const pool = new Pool({connectionString: connectionString,
+                       ssl: true});
+
+pool.connect(err=>console.log(err, "from pool connect"))
+
+client.connect(err=>console.log(err))
 
 pool.on('error', (err,client) => console.log("Pool error:", err))
 
