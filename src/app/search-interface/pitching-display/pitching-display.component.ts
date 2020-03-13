530e5f6f886ca6ae92a0ff03de723ac143f93a61
mikejunt <mikejunt@gmail.com>
2020-03-13 12:53:35 -0500

    duplicates table successes on pitcher table

--- src/app/search-interface/pitching-display/pitching-display.component.ts
+++ src/app/search-interface/pitching-display/pitching-display.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pitcher } from 'src/app/interfaces/pitcher.interface';
 import { AppState } from 'src/app/store';
 import { Store } from '@ngrx/store';
 import * as Selectors from '../../store/selectors';
 import * as Actions from '../../store/actions';
+import * as qclone from 'qclone'
 import { Team } from 'src/app/interfaces/team.interface';
 import { SearchTerms } from 'src/app/interfaces/search.terms.interface';
+import { Sort, MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-pitching-display',
@@ -22,7 +26,10 @@ export class PitchingDisplayComponent implements OnInit {
   teamlist$: Observable<Team[]>
   teamlist: Team[]
   teamview: string = "allteams"
-  searchterms$: Observable<SearchTerms>
+  searchterms$: Observable<SearchTerms>;
+  pitchdata: MatTableDataSource<Pitcher>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator
+  @ViewChild(MatSort, { static: true }) sort: MatSort
 
   constructor(private store: Store<AppState>) {
     this.pitchers$ = this.store.select(Selectors.viewPitching)
@@ -34,9 +41,21 @@ export class PitchingDisplayComponent implements OnInit {
     this.searchterms$.subscribe(res =>{this.ipselect = res.ipfilter;this.teamview = res.teamfilter})
   }
 
+  sortTable(sort:Sort) {
+    console.log(sort)
+  }
+
   ngOnInit(): void {
     let terms: SearchTerms = {searchyear: "2019", posfilter: "all", ipfilter: "100", teamfilter: "allteams", pafilter: "500"}
     this.store.dispatch(Actions.saveSearchTerms({searchterms: terms}))
+    this.teamlist$.subscribe(res => this.teamlist = res);
+    this.searchterms$.subscribe(res =>{this.ipselect = res.ipfilter;this.teamview = res.teamfilter})
+    this.pitchers$.subscribe(hit => {
+      this.pitchers = qclone.qclone(hit);
+        this.pitchdata = new MatTableDataSource(this.pitchers);
+        this.pitchdata.paginator = this.paginator; 
+        this.pitchdata.sort = this.sort
+      })
   }
 
 }
